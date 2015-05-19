6e291ded8fa6ebfe5fe39b1684a8cc57caf7b699
danigb <danigb@gmail.com>
2015-05-19 12:34:47 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var intervalo = require('intervalo');
-var parse = require('note-parse');
+var Interval = require('interval-parser');
+var Note = require('note-parser');
 
 var PITCH_CLASSES = "cdefgabcdefgab";
 var SEMITONES = {c: 0, d: 2, e: 4, f: 5, g: 7, a: 9, b: 11 }
 var ACCIDENTALS = ['bb', 'b', '', '#', '##']
 
 var pitch = function(note) {
-  note = parse(note);
+  note = Note(note);
   var alter = note.accidentals.length;
   if(note.accidentals[0] === 'b') alter = -1 * alter;
   return SEMITONES[note.pitchClass] + alter + 12 * (note.octave + 1);
@@ -30,13 +30,12 @@ pitch.transpose = function(note, interval) {
 }
 
 function transpose(note, interval) {
-  note = parse(note);
-  interval = intervalo(interval);
-  var simple = interval.simple();
+  note = Note(note);
+  interval = Interval(interval);
   var pitchIndex = PITCH_CLASSES.indexOf(note.pitchClass);
-  var pitchClass = PITCH_CLASSES[pitchIndex + simple.number() - 1];
-  var dest = parse(pitchClass + (note.octave + interval.octaves()));
-  var difference = interval.semitones() - (pitch(dest) - pitch(note));
+  var pitchClass = PITCH_CLASSES[pitchIndex + interval.simple - 1];
+  var dest = Note(pitchClass + (note.octave + interval.octaves));
+  var difference = interval.semitones - (pitch(dest) - pitch(note));
   var reduced = difference % 12;
   var octaves = (difference - reduced) / 12;
   var accidentals = ACCIDENTALS[reduced + 2];
